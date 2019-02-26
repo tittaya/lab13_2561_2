171b7f0b1c2f16be4abd5d91363cc6fa151bcc9c
lab314-63@labeng.local <lab314-63@labeng.local>
2019-02-26 17:47:05 +0700

    13.1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,19 +11,40 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
+	int i,j;
+	for(i=1;i<N;i++){
+		for(int k=0;k<N;k++){
+			if(k==i) cout << "[" << d[k] << "]";
+			else cout << d[k] << " ";
+		}
+		int g=0;
+		for(j=0;j<i;j++){
+			if(d[j]<d[i]){
+				g++;
+				swap(d,i,j);
+			}
+		}
+			cout << "==>" << " ";
+			for(int k=0;k<N;k++){
+				if(k==(j-g)) cout << "["<< d[k] << "]";
+				else cout << d[k] << " ";
+			}
+			cout << "\n";
+	}
 
 }
 
+
+
+
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
-	cout << "Input Array:";
+	cout << "Input Array: ";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";
 	cout << "\n\n";
 	
 	insertionSort(a,10);
 	
-	cout << "\nSorted Array:";
+	cout << "\nSorted Array: ";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
